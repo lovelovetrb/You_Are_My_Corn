5dfc22fa0b0e1d67b4067d68eef7193073277026
Maxel31 <sorutongenuine.31@gmail.com>
2023-07-16 14:13:26 +0900

    [add] result.tsx

--- frontend/source/src/pages/result.tsx
+++ frontend/source/src/pages/result.tsx
@@ -85,7 +85,9 @@ const Result = () => {
                         <div>
                             {(() => {
                                 if (result.score == 100) {
-                                    return <h2 className={styles.award}>{`しょーごー: ${num2category(result.category)}のエモちゃれのをきわめしもの`}</h2>;
+                                    return <h2 className={styles.award}>{`しょーごー: ${num2category(result.category)}のエモちゃれ神`}</h2>;
+                                } else if (result.score >= 99) {
+                                    return <h2 className={styles.award}>{`しょーごー: ${num2category(result.category)}のエモちゃれをきわめしもの`}</h2>;
                                 } else if (result.score >= 80) {
                                     return <h2 className={styles.award}>{`しょーごー: ${num2category(result.category)}のエモちゃれのマスター!`}</h2>;
                                 } else if (result.score >= 50) {

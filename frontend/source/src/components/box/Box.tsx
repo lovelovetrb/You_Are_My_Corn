f5a8729f25942dab6ab96c484c5bead100fa9c33
Mizuki <mbaba@kanolab.net>
2023-07-16 13:38:10 +0900

    [fix] if require words 0 -> show text

--- frontend/source/src/components/box/Box.tsx
+++ frontend/source/src/components/box/Box.tsx
@@ -10,6 +10,7 @@ const Box = ({ heading, words }: Props) => {
         <div className={styles.wrapper}>
             <p className={styles.heading}>{heading}</p>
             <ol className={styles.list}>
+                {words[0] === "" && <p>今回の縛りはなし！！好きな言葉を使ってね！！</p>}
                 {words.map((word, index) => {
                     return (
                         <li key={index} className={styles.listItem}>

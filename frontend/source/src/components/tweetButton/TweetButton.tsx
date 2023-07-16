f3a539f778a31f99264a0b6f800fe6a47879fe10
Mizuki <mbaba@kanolab.net>
2023-07-16 16:48:57 +0900

    [fix] change share url

--- frontend/source/src/components/tweetButton/TweetButton.tsx
+++ frontend/source/src/components/tweetButton/TweetButton.tsx
@@ -16,7 +16,7 @@ const TweetButton = ({ text, result, onClickFunc }: props) => {
     return (
         <a
             className={css.button}
-            href={`http://twitter.com/share?url=${location.href}&text=${result.text}%0a私のスコアは！！！${result.score}ダ！！！%0a&hashtags=エモちゃれ`}
+            href={`http://twitter.com/share?url=https://bit.ly/emochare&text=${result.text}%0a私のスコアは！！！${result.score}ダ！！！%0a&hashtags=エモちゃれ`}
             target="_blank"
             rel="noreferrer"
         >
